2b00db6332f5e8b8d4f46e1b2193be29eaf26ef9
nibidhacr <137284985+nibidhacr@users.noreply.github.com>
2023-07-15 11:15:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function myfunction(){
+   alert("thank you for the rating")
+    
+    
+ }
+ function sta1(){
+  
+  document.getElementById("star1").style.color = "yellow";
+ 
+  }
+function sta2()
+{
+  document.getElementById("star1").style.color = "yellow";
+ 
+  document.getElementById("star2").style.color="yellow"
+}
+function sta3()
+{
+  document.getElementById("star1").style.color = "yellow";
+  document.getElementById("star2").style.color="yellow"
+  document.getElementById("star3").style.color="yellow"
+
+}
+function sta4()
+{
+  document.getElementById("star1").style.color = "yellow";
+  document.getElementById("star2").style.color="yellow"
+  document.getElementById("star3").style.color="yellow"
+ 
+  document.getElementById("star4").style.color="yellow"
+
+}
+function sta5()
+{
+  document.getElementById("star1").style.color = "yellow";
+  document.getElementById("star2").style.color="yellow"
+  document.getElementById("star3").style.color="yellow"
+ 
+  document.getElementById("star4").style.color="yellow"
+
+ 
+  document.getElementById("star5").style.color="yellow"
+
+}
